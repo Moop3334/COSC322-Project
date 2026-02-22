27fa60dd92caf9b3bfbae412dc880eed22dd0162
sawyercairns <mrsawyercairns@gmail.com>
2026-02-21 16:07:26 -0800

    Update COSC322Node.java
    Beginning node expansion method

--- src/main/COSC322Node.java
+++ src/main/COSC322Node.java
@@ -16,8 +16,20 @@ public class COSC322Node {
         return g + getHValue();
     }
 
-    public ArrayList<Cosc322Node> expandNode(){
+    public ArrayList<Cosc322Node> expandNode(boolean whitePieces){
         //TODO: Creates a list of nodes containing all possible moves from this node.
+        for (int i = 0; i < 8; i++){
+            for (int j = 0; j < 8; j++){
+                int position = state[i][j];
+                if (position == 0) continue; //skip empty spaces
+                else if (position == (whitePieces? 1:2))//This is a queen of our colour, expand it.
+                {
+
+                }else{ //this is a blocking tile (either an arrow or an opponent queen)
+                    continue; //can probably just skip this too, leaving it separate though just in case.
+                }
+            }
+        }
     }
 
     public int getHvalue(){
